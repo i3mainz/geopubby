2bd63d2209082b16ea83184cb4f3a354f2a397b1
i3mainzuser <46316081+i3mainzuser@users.noreply.github.com>
2020-05-22 10:36:06 +0000

    Deploying to gh-pages from  @ 5ae511d2f67ed8474dd13cc999d62de9ee66cbcc 🚀

--- annotated_dup.js
+++ annotated_dup.js
@@ -1,6 +1,6 @@
 var annotated_dup =
 [
     [ "de", "a00147.html", "a00147" ],
-    [ "Comparable", "a00918.html", null ],
-    [ "RuntimeException", "a00922.html", null ]
+    [ "Comparable", "a00922.html", null ],
+    [ "RuntimeException", "a00926.html", null ]
 ];
