928b8609619f454088e0bb86faad3ad024357eff
timo.homburg <timo.homburg@hs-mainz.de>
2021-01-02 22:55:52 +0100

    GraphML fix

--- src/main/java/de/fuberlin/wiwiss/pubby/exporter/rdf/GraphMLWriter.java
+++ src/main/java/de/fuberlin/wiwiss/pubby/exporter/rdf/GraphMLWriter.java
@@ -33,9 +33,9 @@ public class GraphMLWriter extends GeoModelWriter {
 		Property rdftype=model.createProperty("http://www.w3.org/1999/02/22-rdf-syntax-ns#type");
 		if(res.getURI().contains("http://www.w3.org/2000/01/rdf-schema#")) {
 			return "#F08080";
-		}else if(res.listProperties(scof).hasNext()){
-			return "#F0F8FF";	
 		}else if(res.listProperties(spof).hasNext()){
+			return "#F0F8FF";	
+		}else if(res.listProperties(scof).hasNext()){
 			return "#ffa500";	
 		}else if(res.listProperties(rdftype).hasNext()) {
 			StmtIterator it=res.listProperties(rdftype);
