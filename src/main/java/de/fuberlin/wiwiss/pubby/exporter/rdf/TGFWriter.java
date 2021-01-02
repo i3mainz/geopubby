b1d0b00df5245c252bc9b73d20465efd515ccdd8
timo.homburg <timo.homburg@hs-mainz.de>
2021-01-02 21:19:31 +0100

    TGF fix

--- src/main/java/de/fuberlin/wiwiss/pubby/exporter/rdf/TGFWriter.java
+++ src/main/java/de/fuberlin/wiwiss/pubby/exporter/rdf/TGFWriter.java
@@ -1,7 +1,9 @@
 package de.fuberlin.wiwiss.pubby.exporter.rdf;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 import javax.servlet.http.HttpServletResponse;
@@ -32,20 +34,33 @@ public class TGFWriter extends GeoModelWriter {
                         resources.add(object.asResource());
                     }
         });
+        Map<String,Integer> uriToNodeId=new HashMap<String,Integer>();
         Integer nodecounter=0;
         Integer resnodeid=0;
         StringBuilder edgebuilder=new StringBuilder();
         for(Resource res:resources) {
         	if(!res.isURIResource())
 				continue;
-        	resnodeid=nodecounter++;
+        	if(uriToNodeId.containsKey(res.getURI())) {
+        		resnodeid=uriToNodeId.get(res.getURI());
+        	}else {
+            	resnodeid=nodecounter++;
+            	uriToNodeId.put(res.getURI(),resnodeid);
+        	}
         	response.getWriter().write(resnodeid+" "+res.getURI()+System.lineSeparator());
         	StmtIterator propiter = res.listProperties();
         	while(propiter.hasNext()) {
         		Statement curst=propiter.next();
         		if(curst.getObject().isURIResource()) {
-        			response.getWriter().write(nodecounter+" "+curst.getObject().asResource().getURI()+System.lineSeparator());
-        			edgebuilder.append(resnodeid+" "+nodecounter+++" "+curst.getPredicate().getURI()+System.lineSeparator());
+        			Integer objnodeid=0;
+        			if(uriToNodeId.containsKey(curst.getObject().asResource().getURI())) {
+        				objnodeid=uriToNodeId.get(curst.getObject().asResource().getURI());
+        			}else {
+        				objnodeid=nodecounter;
+        				uriToNodeId.put(curst.getObject().asResource().getURI(),objnodeid);
+        			}
+        			response.getWriter().write(objnodeid+" "+curst.getObject().asResource().getURI()+System.lineSeparator());
+        			edgebuilder.append(resnodeid+" "+objnodeid+" "+curst.getPredicate().getURI()+System.lineSeparator());
         		}else if(curst.getObject().isLiteral()) {
         			response.getWriter().write(nodecounter+" "+curst.getObject().asLiteral().getValue()+" ("+curst.getObject().asLiteral().getDatatypeURI()+")"+System.lineSeparator());
         			edgebuilder.append(resnodeid+" "+nodecounter+++" "+curst.getPredicate().getURI()+System.lineSeparator());
