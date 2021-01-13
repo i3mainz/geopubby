ebf1bcf5c68244d30a720d8f4601f8964da02ad5
timo.homburg <timo.homburg@hs-mainz.de>
2021-01-14 00:01:14 +0100

    Style fixes

--- src/main/java/de/fuberlin/wiwiss/pubby/servlets/PageURLServlet.java
+++ src/main/java/de/fuberlin/wiwiss/pubby/servlets/PageURLServlet.java
@@ -53,7 +53,7 @@ public class PageURLServlet extends BaseServlet {
 		context.put("showLabels", config.showLabels());
 		context.put("geoms",description.getGeoms());
 		context.put("epsg",description.getEPSG());
-		context.put("style",new GeoJSONCSSFormatter().formatForWebView(description.getStyle()).toString().replace("\"", "'"));
+		context.put("styleWithHtml",new GeoJSONCSSFormatter().formatForWebView(description.getStyle()).toString());
 		addPageMetadata(context, controller, description.getModel());
 	
 		template.renderXHTML("page.vm");
