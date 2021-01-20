0be1e8dc0a2eee4f9fea5ba95f18c01a07bd8105
situx <timo.homburg@gmx.de>
2021-01-20 01:11:01 +0100

    Merge

--- src/main/java/de/fuberlin/wiwiss/pubby/util/Condition.java
+++ src/main/java/de/fuberlin/wiwiss/pubby/util/Condition.java
@@ -11,7 +11,29 @@ public class Condition {
 	@Override
 	public String toString() {
 		StringBuilder builder=new StringBuilder();
-		builder.append("sh:property [\n sh:path "+property+";\n");
+		builder.append("sh:condition [\n sh:property [\n sh:path "+property+";\n");
+		switch(operator) {
+		case "PropertyIsEqualTo":
+		case "PropertyIsNotEqualTo":
+		case "PropertyIsLessThan":
+		case "PropertyIsLessThanOrEqualTo":
+		case "PropertyIsGreaterThan":
+		case "PropertyIsGreaterThanOrEqualTo":
+		case "PropertyIsLike":
+		case "PropertyIsNull":
+		case "PropertyIsBetween":
+		case "Intersects":
+		case "Equals":
+		case "Disjoint":
+		case "Touches":
+		case "Within":
+		case "Overlaps":
+		case "Crosses":
+		case "Contains":
+		case "DWithin":
+		case "Beyond":
+		case "BBOX":
+		}
 		builder.append("sh:");
 		return builder.toString();
 	}
