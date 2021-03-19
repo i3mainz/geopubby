9326f40554806df6608cf88be136d25818a6aed2
timo.homburg <timo.homburg@hs-mainz.de>
2021-03-19 22:11:40 +0100

    Added Geobrowser test

---
+++ src/main/java/de/fuberlin/wiwiss/pubby/servlets/GeoBrowserServlet.java
@@ -0,0 +1,40 @@
+package de.fuberlin.wiwiss.pubby.servlets;
+
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.velocity.context.Context;
+
+import de.fuberlin.wiwiss.pubby.Configuration;
+import de.fuberlin.wiwiss.pubby.Dataset;
+
+public class GeoBrowserServlet extends BaseServlet {
+
+	public boolean doGet(String relativeURI,
+			HttpServletRequest request,
+			HttpServletResponse response,
+			final Configuration config) throws ServletException, IOException {
+				
+		VelocityHelper template = new VelocityHelper(getServletContext(), response);
+		Context context = template.getVelocityContext();
+		context.put("project_name", config.getProjectName());
+		context.put("project_link", config.getProjectLink());
+		List<String> sources=new LinkedList<String>();
+		List<String> sourceURLs=new LinkedList<String>();
+		for(Dataset ds:config.getDatasets()) {
+			sources.add(ds.sparqlEndpoint);
+			sourceURLs.add(ds.datasetBase);
+		}
+		context.put("endpoint", sources);
+		context.put("sourceURLs", sourceURLs);
+		context.put("server_base", config.getWebApplicationBaseURI());
+		template.renderXHTML("geobrowser.vm");
+		return true;
+	}
+
+}
