c112a209ba568be37435774a97151104843ec6e7
Timo <situx@users.noreply.github.com>
2024-10-02 17:14:40 +0200

    fixes

--- public/js/epsgdefs.js
+++ public/js/epsgdefs.js
@@ -4850,27 +4850,9 @@ function geometryToGeoJSON(geomtype,coordinates){
 }
 
 function wktToGeoJSON(wktstring){
-	var geomtype=wktstring.split("(")[0].trim()
-	var coordinatestr=wktstring.replaceAll("(","").replaceAll(")","")
-	var coordinates=[]
-	if(coordinatestr.includes(",")){
-		for(coord of coordinatestr.replace(geomtype,"").split(",")){
-			//console.log(coord)
-			spl=coord.split(" ")
-			coordinates.push([spl[0],spl[1]])
-		}
-		if(geomtype.toLowerCase().includes("polygon")){
-			coordinates=[coordinates]
-		}			
-	}else{
-		spl=coordinatestr.replace(geomtype,"").split(" ")
-		coordinates=[spl[0],spl[1]]
-	}
-	if(geomtype.toLowerCase().includes("multi")){
-		geomtype=geomtype.toLowerCase().replace("multi","")
-		geomtype=geomtype.substring(0,1).toUpperCase()+geomtype.substring(1,geomtype.length).toLowerCase()
-	}
-	return {"type":geomtype,"coordinates":coordinates}
+	var wicket = new Wkt.Wkt();
+    wicket.read(wktstring);
+	return wicket.toJson();
 
 }
 
